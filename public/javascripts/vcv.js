e5575d111166c7e5ac437506ed5bde0ce189a37d
surya <surya.surakhman@123rf.com>
2015-03-10 17:20:10 +0700

    re design home page using sublime theme

--- public/javascripts/vcv.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('home',[]);
-
-app.controller('index', ['$scope','$http', function($scope,$http){
-	
-}]);;var app = angular.module('main',[]);
-
-app.controller('register', ['$scope','$http', function($scope,$http){
-	
-}]);
