a44c139b5a232d58f8e7e39e256c91025020dc08
home <surya.ramshere@gmail.com>
2015-03-15 22:52:50 +0700

    redesign register page

--- public/services/common.data.user.js
+++ public/services/common.data.user.js
@@ -8,13 +8,23 @@ angular.module('temporarydata', [])
 
 	return {		
 		set: function(key,value){
-			$cookieStore.put(key,value);
+			if (typeof(Storage) !== "undefined") {
+				localStorage.setItem(key,JSON.stringify(value));
+				console.log("browser support");
+			}else{
+				console.log("browser unsupport");
+				$cookieStore.put(key,value);
+			}
 		},
 		get: function(key){
-			return $cookieStore.get(key);
+			return typeof(Storage) !== "undefined" ? localStorage.getItem(key) : $cookieStore.get(key);			
 		},
 		delete : function(key){
-			$cookieStore.remove(key);
+			if (typeof(Storage) !== "undefined") {
+				localStorage.removeItem(key);
+			}else{
+				$cookieStore.remove(key);
+			}
 		}
 	}
 	
