ad957ee0e888bad2c5604ab43b31acd225db09fb
home <surya.ramshere@gmail.com>
2015-03-31 22:33:16 +0700

    jsonml stringify

--- routes/templating.js
+++ routes/templating.js
@@ -2,6 +2,10 @@ var express = require('express'),
 	router = express.Router(),
 	parser = require('jsonml').parse;
 
+var Stringify = require("jsonml-stringify/stringify");
+var stringify = Stringify([
+    require("jsonml-stringify/plugins/loose")
+]);
 
 router.get('/',function(req,res){
 	res.render('editable-template');
@@ -9,7 +13,10 @@ router.get('/',function(req,res){
 
 router.post('/store', function(req,res){
 	var result = parser(req.body.template);
+
+	var html = stringify(result);
 	console.log(result);
+	console.log(html);
 	res.json({status: 'ok'});
 });
 
